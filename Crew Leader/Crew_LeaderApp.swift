5901d16c8de817debf19aed54fe5c2c03461d839
maxmerc17 <maxmercer36@gmail.com>
2022-09-09 15:06:37 -0400

    Initial Commit

---
+++ Crew Leader/Crew_LeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crew_LeaderApp.swift
+//  Crew Leader
+//
+//  Created by Max Mercer on 2022-09-09.
+//
+
+import SwiftUI
+
+@main
+struct Crew_LeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
